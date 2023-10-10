ae6de447a1861c929c68dac1208196e9bee0e769
João Carli <jthorlief@gmail.com>
2023-10-10 14:53:17 -0300

    feat: Treatment of existing user

--- src/main/java/br/com/jaocarli/todolist/user/UserController.java
+++ src/main/java/br/com/jaocarli/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.jaocarli.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-      System.out.println(userModel.getUsername());
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      System.out.println("User already exists in the database");
+      // mensagem de erro
+      // status code 200
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists in the database");
+    }
+
+      var userCreated = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
