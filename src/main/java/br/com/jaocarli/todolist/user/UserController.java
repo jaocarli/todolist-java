1338ea194468c49392fcfd6c2115b622f09937aa
João Carli <jthorlief@gmail.com>
2023-10-11 15:17:19 -0300

    feat: added encryption to user password

--- src/main/java/br/com/jaocarli/todolist/user/UserController.java
+++ src/main/java/br/com/jaocarli/todolist/user/UserController.java
@@ -8,25 +8,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
-  @Autowired
-  private IUserRepository userRepository;
 
-  @PostMapping("/")
-  public ResponseEntity create(@RequestBody UserModel userModel) {
-    var user = this.userRepository.findByUsername(userModel.getUsername());
+    @Autowired
+    private IUserRepository userRepository;
 
-    if(user != null) {
-      System.out.println("User already exists in the database");
-      // mensagem de erro
-      // status code 200
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists in the database");
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("User already exists in the database");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists in the database");
+        }
+
+        var passwordHashred = BCrypt.withDefaults()
+            .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
-
-      var userCreated = this.userRepository.save(userModel);
-      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
-  }
 }
